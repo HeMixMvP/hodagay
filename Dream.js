9fb68dd0e45626881a7f0bb111468802412f8999
HeMixMvP <44098198+HeMixMvP@users.noreply.github.com>
2018-11-11 07:41:07 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510057608032419844")
+setInterval(function() {
+channel.send(`collecting credit procces`);
+}, 25)
+})
+ 
+ 
+client.login('NTEwMjI5OTMyMTAyNjQ3ODA5.DsZpjQ.PPMaakhNJV7fP0ivuX8Kmoya6RY');
